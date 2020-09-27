5bc4d31ba24b8b4c3f4a6341aa5e16ed916f3ada
minured <minured@qq.com>
2020-09-27 16:31:21 +0800

    获取地址并保存到本地

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,4 +1,5 @@
 // pages/cart/index.js
+import { getSetting, openSetting, chooseAddress } from "../../utils/asyncWX.js";
 Page({
   /**
    * 页面的初始数据
@@ -7,23 +8,61 @@ Page({
     hasAddress: false,
   },
 
-  onAddAddress() {
+  async onAddAddress() {
     console.log("添加收货地址");
     //先判断用户的授权状态 再请求地址权限
-
     //三种情况： 前两种都可以直接请求获取地址
     // 用户已授权  true
     // 用户未授权　undefined
-
     // 用户已拒绝  提示用户手动开启授权
+    try {
+      const res = await getSetting();
+      const scopeAddress = res.authSetting["scope.address"];
+      console.log(scopeAddress);
+      // scopeAddress可能为 undefined
+      if (scopeAddress === false) {
+        await openSetting();
+      }
+      const address = await chooseAddress();
+      console.log(address);
+      wx.setStorageSync("address", address);
+    } catch (error) {
+      console.error(error)
+    }
 
-    wx.getSetting({
-      success: (result) => {
-        console.log(result);
-        let scopeAddress = result.authSetting["scope.address"];
-        console.log(scopeAddress);
-      },
-    });
+    // wx.getSetting({
+    //   // TODO 暂时没法清楚授权数据
+
+    //   success: (result) => {
+    //     console.log(result);
+    //     let scopeAddress = result.authSetting["scope.address"];
+    //     console.log(scopeAddress);
+
+    //     // scopeAddress 可能为 undefined
+    //     if (scopeAddress === false) {
+    //       // 如果是拒绝过 先打开授权设置
+    //       wx.wx.openSetting({
+    //         success: (result)=>{
+    //           console.log(result);
+
+    //           // 再发起授权请求
+    //           wx.chooseAddress({
+    //             success: (result)=>{
+    //               console.log(result);
+    //             },
+    //           });
+    //         },
+    //       });
+    //     } else {
+    //       wx.chooseAddress({
+    //         success: (result)=>{
+    //           console.log(result)
+    //         },
+
+    //       });
+    //     }
+    //   },
+    // });
   },
 
   /**
